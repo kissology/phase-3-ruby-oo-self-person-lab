f6b0ce38baab0a68cf7901cccd1d257fc0c7c2a6
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-14 13:40:41 -0500

    commit tree adf4e6e1f34467cef12f2d8a8bfb1f461c9c2a22

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,55 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+   def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+   end
+
+   def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 10
+
+   end
+   
+   def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 10
+   end
+
+   def happy?
+    self.happiness >= 7
+   end
+
+   def clean?
+    self.hygiene >= 7
+   end
+
+   def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+   end
+
+   def take_bath?
+    self.hygiene =+ 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+   end
+
+   def work_out?
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+   end
+
+
+end
+
+Person
